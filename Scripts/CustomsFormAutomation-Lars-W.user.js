5432f4ad9d15e019d2fd9dbdd7514f3189d56e9c
transmartoblar <51734401+transmartoblar@users.noreply.github.com>
2019-06-12 14:32:24 +0300

    Update CustomsFormAutomation-Lars-W.user.js

--- Scripts/CustomsFormAutomation-Lars-W.user.js
+++ Scripts/CustomsFormAutomation-Lars-W.user.js
@@ -17,18 +17,14 @@ var answers = ["Transmar Ab","0144866-7","018-27011","trafikledning@transmar.fi"
 var button;
 var nextbutton = document.getElementsByClassName("participation-buttons")[0].childNodes[1].childNodes[1];
 
-var getTomorrow = function(){
-    var date = new Date();
-    date.setTime(date.getTime()+ (24 * 60 * 60 * 1000));
-    return date;
-}
 
 var nclick = function(){
     this.surveyParticipationViewModel.doNext();
 }
 
-var putDate = function(d){
+var putDate = function(){
     var date = document.getElementById("Questions_1__Options_0__Answers_0__TextValue");
+    var d = new Date();
     var day = ("0" + d.getDate()).slice(-2);
     var month = ("0" + (d.getMonth() + 1)).slice(-2);
     date.setAttribute("value",day+"."+month+"."+d.getFullYear());
@@ -39,27 +35,20 @@ var page1 = function() {
          var q = document.getElementById(questionIds[i]);
         q.innerHTML += answers[i];
     }
-    putDate(new Date());
+    putDate();
 }
-
 var page2 = function(){
-    var box = document.getElementById("o_cff34507-4242-e811-80d6-ee6c6c76c8f5").click();
+    var box = document.getElementById("o_d0f34507-4242-e811-80d6-ee6c6c76c8f5").checked = "checked";
 }
-
-var page3 = function(){
-    putDate(getTomorrow());
-    p3Fields();
-}
-
-var p3Fields = function(){
-    document.getElementById("o_b8f34507-4242-e811-80d6-ee6c6c76c8f5").click();
-    var val = document.getElementById("Questions_2__Options_1__AttachedTextValue").value = "m/s Fjärdvägen 05:30";
-    document.getElementById("o_d4f34507-4242-e811-80d6-ee6c6c76c8f5").click();
-    document.getElementById("Questions_3__Options_1__AttachedTextValue").value = "ÅS-5077";
-    document.getElementById("Questions_4__Options_0__Answers_0__TextValue").innerHTML = answers[0];
-    document.getElementById("Questions_4__Options_1__Answers_0__TextValue").innerHTML = answers[1];
-    document.getElementById("o_d9f34507-4242-e811-80d6-ee6c6c76c8f5").click();
-    document.getElementById("Questions_5__Options_1__AttachedTextValue").value = "2018-11-19";
+var page4 = function(){
+    document.getElementById("o_b2f34507-4242-e811-80d6-ee6c6c76c8f5").click();
+    var val = document.getElementById("Questions_0__Options_1__AttachedTextValue").value = "m/s Fjärdvägen 15:00";
+    document.getElementById("Questions_1__Options_0__Answers_0__TextValue").innerHTML = answers[0];
+    document.getElementById("Questions_1__Options_1__Answers_0__TextValue").innerHTML = answers[1];
+    document.getElementById("o_bcf34507-4242-e811-80d6-ee6c6c76c8f5").click();
+    document.getElementById("Questions_2__Options_1__AttachedTextValue").value = "ÅS-5077";
+    document.getElementById("o_c7f34507-4242-e811-80d6-ee6c6c76c8f5").click();
+    document.getElementById("Questions_3__Options_1__AttachedTextValue").value = "2018-11-19";
 }
 
 var inputFields = function() {
@@ -68,11 +57,14 @@ var inputFields = function() {
     if(document.getElementById("q_86f34507-4242-e811-80d6-ee6c6c76c8f5")!=undefined){
         page1();
         //nextbutton.addEventListener("onclick", nclick());
-    }else if(document.getElementById("o_cff34507-4242-e811-80d6-ee6c6c76c8f5")!=undefined){
+    }else if(document.getElementById("q_79f34507-4242-e811-80d6-ee6c6c76c8f5")!=undefined){
         page2();
         //nextbutton.addEventListener("onclick", nclick());
-    }else if(document.getElementById("Questions_1__Options_0__Answers_0__TextValue")!=undefined){
-        page3();
+    }else if(document.getElementById("q_66f34507-4242-e811-80d6-ee6c6c76c8f5")!=undefined){
+        putDate();
+        //nextbutton.addEventListener("onclick", nclick());
+    }else if(document.getElementById("q_64f34507-4242-e811-80d6-ee6c6c76c8f5")!=undefined){
+        page4();
         //nextbutton.addEventListener("onclick", nclick());
     }
 }
