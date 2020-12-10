4c7641e26dff648639dd419167535ea6827eadb8
Tobias <tobias.larsson@transmar.fi>
2020-12-10 15:09:18 +0200

    added scrtipfile for LG

--- README.md
+++ README.md
@@ -20,18 +20,16 @@ I Mozilla Firefox:</br>
 ## STEG 2. Installera skriptet till skripthanteraren
  - Klicka på en av skript-länkarna nedan och klicka på Installera enligt bilden. </br>
  ![](res/tampermonkeyInstall.png "Installera")
- 
+
    - [Jouni N](https://github.com/transmartoblar/CustomsFormAutomation/raw/master/Scripts/CustomsFormAutomation-Jouni-N.user.js)
    - [Kai K](https://github.com/transmartoblar/CustomsFormAutomation/raw/master/Scripts/CustomsFormAutomation-Kai-K.user.js)
    - [Lars W](https://github.com/transmartoblar/CustomsFormAutomation/raw/master/Scripts/CustomsFormAutomation-Lars-W.user.js)
+   - [Lucas G](https://github.com/transmartoblar/CustomsFormAutomation/raw/master/Scripts/CustomsFormAutomation-Lucas-G.user.js)
    - [Laura H](https://github.com/transmartoblar/CustomsFormAutomation/raw/master/Scripts/CustomsFormAutomation-Laura-H.user.js)
    - [Markku L](https://github.com/transmartoblar/CustomsFormAutomation/raw/master/Scripts/CustomsFormAutomation-Markku-L.user.js)
    - [Niclas A](https://github.com/transmartoblar/CustomsFormAutomation/raw/master/Scripts/CustomsFormAutomation-Niclas-A.user.js)
-   
+
    - [Björn A](https://github.com/transmartoblar/CustomsFormAutomation/raw/master/Scripts/CustomsFormAutomation-Bjorn-A.user.js)
    - [Tommy E](https://github.com/transmartoblar/CustomsFormAutomation/raw/master/Scripts/CustomsFormAutomation-Tommy-E.user.js)
    - [Bo E](https://github.com/transmartoblar/CustomsFormAutomation/raw/master/Scripts/CustomsFormAutomation-Bo-E.user.js)
    - [David B](https://github.com/transmartoblar/CustomsFormAutomation/raw/master/Scripts/CustomsFormAutomation-David-B.user.js)
-   
-   
-   
---
+++ Scripts/CustomsFormAutomation-Lucas-G.user.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name         CustomsFormAutomation
+// @namespace    http://tampermonkey.net/
+// @version      1.3
+// @description  Automating the static fields of the form
+// @author       toblar
+// @match        https://link.webropolsurveys.com/Participation/Public/63d02a36-566e-42a5-b948-0ea27a779ca7?displayId=Fin1520863
+// @match        https://link.webropolsurveys.com/Participation/Public/63d02a36-566e-42a5-b948-0ea27a779ca7?displayId=Fin1520863&surveyLocale=sv
+// @match        https://link.webropolsurveys.com/Participation/Public/f2f8fee1-1430-4c3d-939a-0d6dd0b399dc?displayId=Fin2066775&surveyLocale=sv
+// @match        https://link.webropolsurveys.com/Participation/Public/f2f8fee1-1430-4c3d-939a-0d6dd0b399dc?displayId=Fin2066775
+// ==/UserScript==
+
+
+var name = "Lucas";
+var surname = "Granholm";
+var telnr = "018-27011";
+var epostUser = "lucas.granholm@transmar.fi";
+var companyname = "Transmar Ab";
+var foNr = "0144866-7";
+var companyTelnr = "018-27011";
+var companyEpost = "trafikledning@transmar.fi";
+var manifestNr = "2018-11-19";
+
+var nclick = function(){
+    location.href = "javascript:void(surveyParticipationViewModel.doNext());";
+}
+
+var getDate = function(){
+    var d = new Date();
+    var day = ("0" + d.getDate()).slice(-2);
+    var month = ("0" + (d.getMonth() + 1)).slice(-2);
+    return day+"."+month+"."+d.getFullYear();
+}
+
+
+var page2 = function(){
+    document.getElementById("Questions_0__Options_0__Answers_0__TextValue").innerHTML = name;
+    document.getElementById("Questions_0__Options_1__Answers_0__TextValue").innerHTML = surname;
+    document.getElementById("Questions_0__Options_2__Answers_0__TextValue").innerHTML = telnr;
+    document.getElementById("Questions_0__Options_3__Answers_0__TextValue").innerHTML = epostUser;
+
+    document.getElementById("Questions_1__Options_0__Answers_0__TextValue").innerHTML = companyname;
+    document.getElementById("Questions_1__Options_1__Answers_0__TextValue").innerHTML = foNr;
+    document.getElementById("Questions_1__Options_2__Answers_0__TextValue").innerHTML = companyTelnr;
+    document.getElementById("Questions_1__Options_3__Answers_0__TextValue").innerHTML = companyEpost;
+    document.getElementById("Questions_1__Options_4__Answers_0__TextValue").innerHTML = manifestNr;
+
+    document.getElementById("Questions_2__Options_0__Answers_0__TextValue").setAttribute("value",getDate());
+}
+
+var inputFields = function() {
+    'use strict';
+    console.log("Fyller i fält");
+    if(document.getElementById("o_9d479a78-b795-4588-86c7-fa57de48f9ab")!=undefined){
+        document.getElementById("o_9d479a78-b795-4588-86c7-fa57de48f9ab").click();
+        document.querySelector("#q_8886a460-9bda-4b33-8e83-404254b00fbf > div > div:nth-child(1)").click();
+
+        setTimeout(nclick(),500);
+
+    }else if(document.getElementById("Questions_0__Options_0__Answers_0__TextValue")!=undefined){
+        page2();
+    }
+}
+
+document.addEventListener("onload", inputFields());
